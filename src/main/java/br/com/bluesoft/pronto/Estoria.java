69a8d1918e760cfa8aa2ef279b13ecbbf52c7bdf
Andre Faria Gomes <andre.java@gmail.com>
2009-05-21 21:21:42 -0300

    Criando Objetos de Modelo

---
+++ src/main/java/br/com/bluesoft/pronto/Estoria.java
@@ -0,0 +1,38 @@
+package br.com.bluesoft.pronto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Estoria {
+
+	@Id
+	private int estoriaKey;
+	private String titulo;
+	private String descricao;
+
+	public int getEstoriaKey() {
+		return estoriaKey;
+	}
+
+	public void setEstoriaKey(int estoriaKey) {
+		this.estoriaKey = estoriaKey;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
--- src/main/java/br/com/bluesoft/pronto/UserStory.java
+++
@@ -1,6 +0,0 @@
-package br.com.bluesoft.pronto;
-
-
-public class UserStory {
-
-}
