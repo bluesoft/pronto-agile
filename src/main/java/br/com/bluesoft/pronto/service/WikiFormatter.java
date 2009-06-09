fcdae41347e5f06c3d6ee8dd4c34f383136e82f2
André Faria Gomes <andrefariagomes@iMac-do-Bento-5.local>
2009-06-09 22:07:25 +0800

    Descricao do Ticket na lista de tickets com Modal
    Signed-off-by: Luiz Faias <luizfaias@gmail.com>

--- src/main/java/br/com/bluesoft/pronto/service/WikiFormatter.java
+++ src/main/java/br/com/bluesoft/pronto/service/WikiFormatter.java
@@ -2,13 +2,20 @@ package br.com.bluesoft.pronto.service;
 
 import info.bliki.wiki.model.WikiModel;
 
+import org.springframework.web.util.JavaScriptUtils;
+
 public class WikiFormatter {
 
 	private static final String wikiImages = "http://www.mywiki.com/wiki/${image}";
 	private static final String wikiTitles = "http://www.mywiki.com/wiki/${title}";
 
-	public static String toHtml(String wiki) {
-		WikiModel wikiModel = new WikiModel(wikiImages, wikiTitles);
+	public static String toHtml(final String wiki) {
+		final WikiModel wikiModel = new WikiModel(wikiImages, wikiTitles);
 		return wikiModel.render(wiki);
+	}
+
+	public static String toHtmlJavaScriptEscaped(final String wiki) {
+		final WikiModel wikiModel = new WikiModel(wikiImages, wikiTitles);
+		return JavaScriptUtils.javaScriptEscape(wikiModel.render(wiki));
 	}
 }
