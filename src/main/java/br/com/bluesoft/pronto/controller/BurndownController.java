ea188c8191805f1e7688d32f904cf9c43f97f4fb
André Faria Gomes <andrefariagomes@iMac-do-Bento-5.local>
2009-06-11 02:14:44 +0800

    burndown com dados de ideal e realizado
    Signed-off-by: Luiz Faias <luizfaias@gmail.com>

--- src/main/java/br/com/bluesoft/pronto/controller/BurndownController.java
+++ src/main/java/br/com/bluesoft/pronto/controller/BurndownController.java
@@ -1,5 +1,6 @@
 package br.com.bluesoft.pronto.controller;
 
+import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -51,8 +52,17 @@ public class BurndownController {
 		final List<Double> burnValues = new LinkedList<Double>();
 		burnValues.add(burnNumber);
 		for (final Double esforco : mapaEsforcoPorDia.values()) {
-			burnNumber = burnNumber - esforco;
+			burnNumber = new BigDecimal(burnNumber - esforco).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 			burnValues.add(burnNumber);
+		}
+
+		burnNumber = esforcoTotal;
+		final double media = esforcoTotal / mapaEsforcoPorDia.values().size();
+		final List<Double> idealValues = new LinkedList<Double>();
+		idealValues.add(burnNumber);
+		for (int i = 0; i < mapaEsforcoPorDia.values().size(); i++) {
+			burnNumber = new BigDecimal(burnNumber - media).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			idealValues.add(burnNumber);
 		}
 
 		final JSONObject raiz = new JSONObject();
@@ -61,27 +71,35 @@ public class BurndownController {
 		title.set("text", "Sprint " + sprint.getNome());
 		raiz.put("title", title);
 
-		// JSONObject y_legend = new JSONObject();
-		// y_legend.set("text", "Y Legend");
-		// raiz.put("y_legend", y_legend);
-
-		// final JSONObject x_legend = new JSONObject();
-		// x_legend.set("text", "Dias");
-		// raiz.put("x_legend", x_legend);
-
 		final JSONArray elements = new JSONArray();
 
 		final JSONObject element1 = new JSONObject();
-		element1.set("type", "line");
-		element1.set("text", "Burndown");
-		element1.set("values", new JSONArray(burnValues));
+		element1.set("type", "area");
+		element1.set("width", "2");
+		element1.set("colour", "#FF0000");
+
+		element1.set("fill", "#E01B49");
+		element1.set("fill-alpha", "0.4");
+
+		final JSONObject dotStyle = new JSONObject();
+		dotStyle.set("type", "hollow-dot");
+		element1.set("dotStyle", dotStyle);
+
+		element1.set("text", "Ideal");
+		element1.set("values", new JSONArray(idealValues));
 		elements.put(element1);
 
-		// JSONObject element2 = new JSONObject();
-		// element2.set("type", "bar");
-		// element2.set("text", "Nao sei");
-		// element2.set("values", new JSONArray(new int[]{9,5,4,3,6,7,8,8,9}));
-		// elements.put(element2);
+		final JSONObject element2 = new JSONObject();
+		element2.set("type", "area");
+		element2.set("width", "2");
+		element2.set("colour", "#00666cc");
+
+		element2.set("fill", "#0066cc");
+		element2.set("fill-alpha", "0.4");
+
+		element2.set("text", "Realizado");
+		element2.set("values", new JSONArray(burnValues));
+		elements.put(element2);
 
 		raiz.put("elements", elements);
 
