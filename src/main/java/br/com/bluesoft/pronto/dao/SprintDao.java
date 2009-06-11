48c8d9519b8150f9cfc1cf8f2c1e6297b8e3ad64
andrefaria <andre.java@gmail.com>
2009-06-12 03:39:16 +0800

    Correcoes nos Totais de Esforco e Valor de Negocio
    Signed-off-by: Luiz Faias <luizfaias@gmail.com>

--- src/main/java/br/com/bluesoft/pronto/dao/SprintDao.java
+++ src/main/java/br/com/bluesoft/pronto/dao/SprintDao.java
@@ -25,6 +25,13 @@ public class SprintDao extends DaoHibernate<Sprint, Integer> {
 	}
 
 	@Override
+	public Sprint obter(final Integer key) {
+		final Sprint sprint = super.obter(key);
+		preencheTotaisDeEsforcoEValorDeNegocioDoSprint(sprint);
+		return sprint;
+	}
+
+	@Override
 	@SuppressWarnings("unchecked")
 	public List<Sprint> listar() {
 		final Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Sprint.class);
@@ -38,7 +45,9 @@ public class SprintDao extends DaoHibernate<Sprint, Integer> {
 	public List<Sprint> listarSprintsEmAberto() {
 		final Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Sprint.class);
 		criteria.add(Restrictions.eq("fechado", false));
-		return criteria.list();
+		final List<Sprint> sprints = criteria.list();
+		preencheTotaisDeEsforcoEValorDeNegocioDosSprints(sprints);
+		return sprints;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -63,20 +72,41 @@ public class SprintDao extends DaoHibernate<Sprint, Integer> {
 		}
 	}
 
+	private void preencheTotaisDeEsforcoEValorDeNegocioDoSprint(final Sprint sprint) {
+		final String sql = "select sprint, sum(t.valor_de_negocio) as valor_de_negocio_total, sum(t.esforco) as esforco_total from ticket t where t.sprint = :sprint group by sprint";
+		final SQLQuery query = getSession().createSQLQuery(sql);
+		query.setInteger("sprint", sprint.getSprintKey());
+		query.addScalar("sprint", Hibernate.INTEGER);
+		query.addScalar("valor_de_negocio_total", Hibernate.INTEGER);
+		query.addScalar("esforco_total", Hibernate.DOUBLE);
+		final Object[] o = (Object[]) query.uniqueResult();
+		final int valorDeNegocioTotal = (Integer) o[1];
+		final double esforcoTotal = (Double) o[2];
+		sprint.setEsforcoTotal(esforcoTotal);
+		sprint.setValorDeNegocioTotal(valorDeNegocioTotal);
+
+	}
+
 	public Sprint getSprintAtual() {
 		final Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Sprint.class);
 		criteria.add(Restrictions.eq("atual", true));
-		return (Sprint) criteria.uniqueResult();
+		final Sprint sprint = (Sprint) criteria.uniqueResult();
+		preencheTotaisDeEsforcoEValorDeNegocioDoSprint(sprint);
+		return sprint;
 	}
 
 	public Sprint getSprintAtualComTickets() {
 		final String hql = "select distinct s from Sprint s left join fetch s.tickets t left join fetch t.filhos f where s.atual = true";
-		return (Sprint) getSession().createQuery(hql).uniqueResult();
+		final Sprint sprint = (Sprint) getSession().createQuery(hql).uniqueResult();
+		preencheTotaisDeEsforcoEValorDeNegocioDoSprint(sprint);
+		return sprint;
 	}
 
 	public Sprint obterSprintComTicket(final Integer sprintKey) {
 		final String hql = "select distinct s from Sprint s left join fetch s.tickets t left join fetch t.filhos f where s.sprintKey = :sprintKey";
-		return (Sprint) getSession().createQuery(hql).setInteger("sprintKey", sprintKey).uniqueResult();
+		final Sprint sprint = (Sprint) getSession().createQuery(hql).setInteger("sprintKey", sprintKey).uniqueResult();
+		preencheTotaisDeEsforcoEValorDeNegocioDoSprint(sprint);
+		return sprint;
 	}
 
 }
