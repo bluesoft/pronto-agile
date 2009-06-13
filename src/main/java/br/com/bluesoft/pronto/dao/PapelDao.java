1b31b317f9e5d798b15cb62be489517d6cba9c20
luizfaias <luizfaias@gmail.com>
2009-06-13 13:07:33 -0300

    Ordenando papéis ao listar

--- src/main/java/br/com/bluesoft/pronto/dao/PapelDao.java
+++ src/main/java/br/com/bluesoft/pronto/dao/PapelDao.java
@@ -1,5 +1,8 @@
 package br.com.bluesoft.pronto.dao;
 
+import java.util.List;
+
+import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
 
 import br.com.bluesoft.pronto.core.Papel;
@@ -9,5 +12,11 @@ public class PapelDao extends DaoHibernate<Papel, Integer> {
 
 	public PapelDao() {
 		super(Papel.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Papel> listar() {
+		return getSession().createCriteria(Papel.class).addOrder(Order.asc("papelKey")).list();
 	}
 }
