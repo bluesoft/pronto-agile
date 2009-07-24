e1751f6fd7ff76ebbe28731559eb0756bc0562ed
luizfaias <luizfaias@gmail.com>
2009-07-24 00:12:18 -0300

    Adicionando mensagem da licença GPL em todas as classes

--- src/main/java/br/com/bluesoft/pronto/controller/WikiController.java
+++ src/main/java/br/com/bluesoft/pronto/controller/WikiController.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2009 Pronto Agile Project Management.
+ *
+ * This file is part of Pronto.
+ *
+ * Pronto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pronto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pronto. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package br.com.bluesoft.pronto.controller;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,9 +30,8 @@ import br.com.bluesoft.pronto.service.WikiFormatter;
 @Controller
 public class WikiController {
 
-
 	@RequestMapping("/wiki/parse.action")
-	public String parse(HttpServletResponse response, String data) throws Exception {
+	public String parse(final HttpServletResponse response, final String data) throws Exception {
 		response.getOutputStream().print(WikiFormatter.toHtml(data));
 		return null;
 	}
