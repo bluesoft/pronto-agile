56282548cd15542c611b98b2bef72788363c9103
luizfaias <luizfaias@gmail.com>
2009-07-24 11:12:18 +0800

    Adicionando mensagem da licença GPL em todas as classes
    Signed-off-by: André Faria Gomes <andre.java@gmail.com>

--- src/main/java/br/com/bluesoft/pronto/ProntoException.java
+++ src/main/java/br/com/bluesoft/pronto/ProntoException.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2009 Pronto Agile Project Management.
+ *
+ * This file is part of Pronto.
+ *
+ * Pronto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pronto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pronto. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package br.com.bluesoft.pronto;
 
 public class ProntoException extends Exception {
