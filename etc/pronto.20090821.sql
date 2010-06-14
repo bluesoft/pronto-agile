60e0f305370d61893031d5c3dfe1b321316fb08e
Luiz Alberto Ferreira <china@iMac-do-China.local>
2010-06-14 18:38:53 -0300

    Unificando arquivos de update de banco de dados em um unico para facilitar a execucao e criacao de novas instancias

--- etc/pronto.20090821.sql
+++
@@ -1,1 +0,0 @@
-alter table ticket add prioridade integer;
--- etc/pronto.20091001.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_ticket_cliente ON ticket USING btree (cliente_key);
-CREATE INDEX idx_ticket_branch ON ticket USING btree (branch);
-CREATE INDEX idx_execucao_script ON execucao USING btree (script_key);
-CREATE INDEX idx_execucao_banco_de_dados ON execucao USING btree (banco_de_dados_key);
