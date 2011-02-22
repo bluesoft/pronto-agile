9f5bd708fb5ca065a889586f62d1780d63ba6d2b
luizfaias <luizfaias@gmail.com>
2011-02-22 13:45:56 -0300

    Adicionando ordenação na lista de categorias

--- src/main/groovy/br/com/bluesoft/pronto/dao/CategoriaDao.groovy
+++ src/main/groovy/br/com/bluesoft/pronto/dao/CategoriaDao.groovy
@@ -1,14 +1,30 @@
 package br.com.bluesoft.pronto.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria
+import org.hibernate.criterion.Order
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import br.com.bluesoft.pronto.model.Categoria;
+import br.com.bluesoft.pronto.model.Sprint;
 
 @Repository 
 class CategoriaDao extends DaoHibernate {
-	
+
+	@Autowired
+	private SessionFactory sessionFactory
+		
 	CategoriaDao() {
 		super(Categoria.class)
 	}
-	
+
+	@Override
+	public List<Categoria> listar() {
+		final Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Categoria.class)
+		criteria.addOrder(Order.asc("descricao"))
+		return criteria.list()
+	}
 }
