9c4ecba57b40927bb0d70befc88ac94b807bd8db
André Faria <andre.java@gmail.com>
2011-07-28 11:13:02 -0300

    Mantendo título dos e-mails para ficarem na mesma thread no Gmail

--- src/main/groovy/br/com/bluesoft/pronto/service/MessageFacade.groovy
+++ src/main/groovy/br/com/bluesoft/pronto/service/MessageFacade.groovy
@@ -26,28 +26,28 @@ class MessageFacade {
 	}
 
 	boolean notificarMovimentacao(MovimentoKanban movimento) {
-		def sbj = "${Seguranca.usuario} movimentou o ticket #${movimento.ticket.ticketKey}";
+		def sbj = "#${movimento.ticket.ticketKey} #{movimento.ticket.titulo}"
 		def url = configuracaoDao.getProntoUrl() + "tickets/${movimento.ticket.ticketKey}"
 		def msg = "${Seguranca.usuario} moveu o ticket #${movimento.ticket.ticketKey} para ${movimento.kanbanStatus.descricao}\n${url}"
 		return this.enviarMensagem(sbj, msg, movimento.ticket.envolvidos)
 	}
 
 	boolean enviarComentario(int ticketKey, String comentario, def to) {
-		def sbj = "${Seguranca.usuario} comentou no ticket #${ticketKey}";
+		def sbj = "#${ticketKey} #{ticket.titulo}"
 		def url = configuracaoDao.getProntoUrl() + "tickets/${ticketKey}"
 		def msg = "${Seguranca.usuario} comenta\n\n ${comentario}\n\n${url}"
 		return this.enviarMensagem(sbj, msg, to)
 	}
 
 	void notificarImpedimento(Ticket ticket) {
-		def sbj = "#${ticket.ticketKey} foi Impedido para ${ticket.responsavel} desimpedir"
+		def sbj = "#${ticket.ticketKey} #{ticket.titulo}"
 		def url = configuracaoDao.getProntoUrl() + "tickets/${ticket.ticketKey}"
 		def msg = "O ticket #${ticket.ticketKey} foi Impedido por ${Seguranca.usuario}, ${ticket.responsavel} é o responsável por desimpedí-lo.\n\n${url}"
 		this.enviarMensagem(sbj, msg, ticket.getTodosOsEnvolvidos());
 	}
 
 	void notificarDesimpedimento(Ticket ticket, def to) {
-		def sbj = "#${ticket.ticketKey} foi Desimpedido"
+		def sbj = "#${ticket.ticketKey} #{ticket.titulo}"
 		def url = configuracaoDao.getProntoUrl() + "tickets/${ticket.ticketKey}"
 		def msg = "O ticket #${ticket.ticketKey} foi Desimpedido por ${Seguranca.usuario}.\n\n${url}"
 		this.enviarMensagem(sbj, msg, to);
