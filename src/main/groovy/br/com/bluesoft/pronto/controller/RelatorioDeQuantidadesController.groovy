51d9fa363f79953745291387e907051985a28c7e
André Faria <andre.java@gmail.com>
2012-04-26 14:08:28 -0300

    Relatório de Quantidades

---
+++ src/main/groovy/br/com/bluesoft/pronto/controller/RelatorioDeQuantidadesController.groovy
@@ -0,0 +1,113 @@
+package br.com.bluesoft.pronto.controller
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import br.com.bluesoft.pronto.dao.RelatorioDeQuantidadesDao
+
+import br.com.bluesoft.pronto.web.binding.DefaultBindingInitializer 
+
+@Controller
+@RequestMapping("/relatorios/quantidades")
+class RelatorioDeQuantidadesController {
+	
+	final static int MAXIMO_DE_GRUPOS = 12
+	final static String VIEW = "/relatorios/quantidades/index.jsp"
+	final static cores = ['AFD8F8','F6BD0F','8BBA00','FF8E46','008E8E','D64646','8E468E','588526','B3AA00','008ED6','9D080D','A186BE']
+	
+	@Autowired
+	RelatorioDeQuantidadesDao relatorioDeQuantidadesDao
+	
+	@InitBinder
+	public void initBinder(final WebDataBinder binder, final WebRequest webRequest) {
+		def defaultBindingInitializer = new DefaultBindingInitializer()
+		defaultBindingInitializer.initBinder binder, webRequest
+	}
+	
+	@RequestMapping
+	String index(Model model){
+		model.addAttribute 'dataInicial', new Date() - 31
+		model.addAttribute 'dataFinal', new Date()
+		return VIEW
+	}
+	
+	@RequestMapping("/gerar.xml")
+	void gerar(Date dataInicial, Date dataFinal, String tipoDeRelatorio, Integer tipoDeTicketKey, HttpServletResponse response){
+		
+		def dados = null
+		switch (tipoDeRelatorio) {
+			case "sprint":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorSprint(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(sintetizar(dados))
+				break;
+			case "categoria":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorCategoria(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(sintetizar(dados))
+				break;
+			case "modulo":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorModulo(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(sintetizar(dados))
+				break;
+			case "semana":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorSemana(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(dados)
+				break;
+			case "mes":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorMes(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(dados)
+				break;
+			case "ano":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorAno(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(dados)
+				break;
+			case "cliente":
+				dados = relatorioDeQuantidadesDao.listarDefeitosPorCliente(dataInicial,dataFinal,tipoDeTicketKey)
+				dados = getJSON(sintetizar(dados))
+				break;
+		}
+		
+		response.setContentType("text/xml")
+		response.writer.write dados
+	}
+	
+	def getJSON(def defeitos){
+		def writer = new StringWriter()
+		def builder = new groovy.xml.MarkupBuilder(writer)
+		int showNameFactor = (defeitos.size() / 12) as Integer
+		showNameFactor = showNameFactor == 0 ? 1 : showNameFactor
+		int showValues = defeitos.size() > 30 ? 0 : 1
+		builder.'graph'(yAxisName:'Demanda (Quantidades)',caption:'Tickets', rotateNames:'1',showValues:showValues) {
+			defeitos.eachWithIndex { defeito, index  ->
+				String color = defeitos.size() < 50  ? (index < 12 ? cores[index] : cores[index % 12]) : cores[0]
+				int showName = index % showNameFactor == 0 ? 1 : 0
+				'set'(name:defeito[0],value:defeito[1],color:color,showName:showName){}
+			}
+		}
+		writer.toString()
+	}
+	
+	def sintetizar(defeitos) {
+		def numerosDeGrupos = defeitos.size()
+		if (numerosDeGrupos > MAXIMO_DE_GRUPOS) {
+			def sinteze = []
+			def outros = 0
+			defeitos.eachWithIndex { it, index ->
+				if (index < MAXIMO_DE_GRUPOS) {
+					sinteze[index] = defeitos[index]
+				} else {
+					outros += defeitos[index][1]
+				}
+			}
+			sinteze[MAXIMO_DE_GRUPOS] = ['Outros', outros]
+			return sinteze
+		} else {
+			return defeitos
+		}
+	}
+}
---
+++ src/main/webapp/relatorios/quantidades/index.jsp
@@ -0,0 +1,62 @@
+<%@ include file="/commons/taglibs.jsp"%>
+<html>
+	<head>
+		<title>Relatório de Quantidades</title>
+	</head>
+	<body>
+		<h1>
+			Relatório de Quantidades
+		</h1>
+		
+		<div id="topo">
+			Tipo de Gráfico:
+			<select id="tipoDeRelatorio" name="tipoDeRelatorio">
+				<option value="semana" chart="FCF_Line">Por Semana</option>
+				<option value="mes" chart="FCF_Line">Por Mês</option>
+				<option value="ano" chart="FCF_Line">Por Ano</option>
+				<option value="categoria" chart="FCF_Bar2D">Por Categoria</option>
+				<option value="cliente" chart="FCF_Bar2D">Por Cliente</option>
+				<option value="modulo" chart="FCF_Bar2D">Por Módulo</option>
+				<option value="sprint" chart="FCF_Bar2D">Por Sprint</option>
+			</select>
+			
+			Tipo de Ticket:
+			<select id="tipoDeTicketKey" name="tipoDeTicketKey">
+				<option value="-1">Tudo</option>
+				<option value="2">Estórias</option>
+				<option value="3">Defeitos</option>
+			</select>
+			
+			<fmt:formatDate var="strDataInicial" value="${dataInicial}"/>
+			Data Inicial: <input type="text" id="dataInicial" class="required dateBr" value="${strDataInicial}" size="12"/>
+			<fmt:formatDate var="strDataFinal" value="${dataFinal}"/>
+			Data Final: <input type="text" id="dataFinal" class="required dateBr" value="${strDataFinal}" size="12"/>
+			
+			&nbsp; &nbsp;
+			
+			<button type="button" onclick="gerar()">Gerar</button>
+		</div>
+		
+		<div id="chartdiv" align="center" style="height: 510px;">
+		</div>
+		<script type="text/javascript">
+			function gerar() {
+				var di = $('#dataInicial').val();
+				var df = $('#dataFinal').val();
+				var tipoDeRelatorio = $("#tipoDeRelatorio").val();
+				var tipoDeTicketKey = $("#tipoDeTicketKey").val();
+				var parametros = "?dataInicial=" + di + "&dataFinal=" + df +"&tipoDeRelatorio=" + tipoDeRelatorio + "&tipoDeTicketKey=" + tipoDeTicketKey;
+				var url = encodeURIComponent("${raiz}relatorios/quantidades/gerar.xml" + parametros);
+				var chartType = $('#tipoDeRelatorio').find('option:selected').attr('chart');
+				var chart = new FusionCharts("${raiz}commons/charts/"+chartType+".swf", "chart", "920", "500");
+				chart.setDataURL(url);
+				chart.render("chartdiv");	
+			}
+
+			$(function(){
+				$('#formSprint').validate();
+				$('.dateBr').datepicker();
+			});
+		</script> 
+	</body>
+</html>
